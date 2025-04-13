61082ecac0d938f938e355fe2f1513ae18878f09
Akinolu <ainaakinolu@gmail.com>
2025-04-13 18:26:35 +0100

    End of values and variables

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
+console.log(40 + 8 + 23 - 10);
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 -10);
+console.log('JONAS');
+console.log(23);
+
+let firstName  = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
