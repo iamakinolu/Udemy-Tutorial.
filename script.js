61437ce2402117d6f17dc94d6e391648baea9ced
Akinolu <ainaakinolu@gmail.com>
2025-05-16 15:02:21 +0100

    Truthsy and Falsy values

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ lines');
 console.log(`String
 multiple
 lines`);
-*/
+
 
 // const age = 15;
 
@@ -181,21 +181,43 @@ lines`);
 // }
 
 // type conversion
-const inputYear  = "1991";
+const inputYear = "1991";
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
 
-console.log(Number('Jonas'));
+console.log(Number("Jonas"));
 console.log(typeof NaN);
 
 console.log(String(1991), 23);
 
 //type coercion
-console.log('I am ' + 23 + ' years old') 
-console.log('23' -'10' - 3);
-console.log('23' * '2' )
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" * "2");
 
-let n = '1' + 1;
+let n = "1" + 1;
 n -= 1;
 console.log(n);
+*/
 
+// 5 falsy values: 0, '', undefined, null, NaN.
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 1;
+if (money) {
+  console.log("Don't spend it all");
+} else {
+  console.log("You should get a job.");
+}
+
+let height = 0;
+if (height) {
+  console.log("You are tall.");
+} else {
+  console.log("You are exitentialy short!");
+}
